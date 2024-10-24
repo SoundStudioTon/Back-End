619cf82cc8c4ac822c47f9d78ab34d7fc9c0aae2
xeulbn <gsbtiger0215@gmail.com>
2024-10-25 00:58:32 +0900

    이미지 프론트에서 ai로 보내기

---
+++ src/main/java/org/example/soundstudiodemo/controller/ImageController.java
@@ -0,0 +1,52 @@
+package org.example.soundstudiodemo.controller;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+public class ImageController {
+
+    @PostMapping("/uploadImage")
+    public ResponseEntity<String> handleImageUpload(@RequestParam("file") MultipartFile file) {
+        try{
+            String aiResponse = sendImageToAiModel(file);
+
+            return ResponseEntity.ok(aiResponse);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이미지 업로드에 실패하였습니다.");
+        }
+    }
+
+    private String sendImageToAiModel(MultipartFile file) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file",new ByteArrayResource(file.getBytes()){
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        });
+
+        HttpEntity<MultiValueMap<String,Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        String aiServerUrl = "";
+        ResponseEntity<String> response = restTemplate.postForEntity(aiServerUrl, requestEntity, String.class);
+
+        return response.getBody();
+    }
+
+
+}
